e794aa2de3c670c4940b8b280933b31703b209ca
Nakshatra6350 <nakshatragarg678@gmail.com>
2024-02-14 13:57:02 +0530

    updated files

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,51 +42,51 @@ var typingEffect = new Typed(".typedText",{
 
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-const sr = ScrollReveal({
-      origin: 'top',
-      distance: '80px',
-      duration: 2000,
-      reset: true     
-})
+// const sr = ScrollReveal({
+//       origin: 'top',
+//       distance: '80px',
+//       duration: 2000,
+//       reset: true     
+// })
 
 /* -- HOME -- */
-sr.reveal('.featured-text-card',{})
-sr.reveal('.featured-name',{delay: 100})
-sr.reveal('.featured-text-info',{delay: 200})
-sr.reveal('.featured-text-btn',{delay: 200})
-sr.reveal('.social_icons',{delay: 200})
-sr.reveal('.featured-image',{delay: 300})
+// sr.reveal('.featured-text-card',{})
+// sr.reveal('.featured-name',{delay: 100})
+// sr.reveal('.featured-text-info',{delay: 200})
+// sr.reveal('.featured-text-btn',{delay: 200})
+// sr.reveal('.social_icons',{delay: 200})
+// sr.reveal('.featured-image',{delay: 300})
 
 
 /* -- PROJECT BOX -- */
-sr.reveal('.project-box',{interval: 200})
+// sr.reveal('.project-box',{interval: 200})
 
 /* -- HEADINGS -- */
-sr.reveal('.top-header',{})
+// sr.reveal('.top-header',{})
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
 /* -- ABOUT INFO & CONTACT INFO -- */
-const srLeft = ScrollReveal({
-origin: 'left',
-distance: '80px',
-duration: 2000,
-reset: true
-})
+// const srLeft = ScrollReveal({
+// origin: 'left',
+// distance: '80px',
+// duration: 2000,
+// reset: true
+// })
 
-srLeft.reveal('.about-info',{delay: 100})
-srLeft.reveal('.contact-info',{delay: 100})
+// srLeft.reveal('.about-info',{delay: 100})
+// srLeft.reveal('.contact-info',{delay: 100})
 
 /* -- ABOUT SKILLS & FORM BOX -- */
-const srRight = ScrollReveal({
-origin: 'right',
-distance: '80px',
-duration: 2000,
-reset: true
-})
+// const srRight = ScrollReveal({
+// origin: 'right',
+// distance: '80px',
+// duration: 2000,
+// reset: true
+// })
 
-srRight.reveal('.skills-box',{delay: 100})
-srRight.reveal('.form-control',{delay: 100})
+// srRight.reveal('.skills-box',{delay: 100})
+// srRight.reveal('.form-control',{delay: 100})
 
 
 
@@ -128,3 +128,5 @@ function SendMail(){
     alert("Success" + res.status)
   })
 }
+
+
